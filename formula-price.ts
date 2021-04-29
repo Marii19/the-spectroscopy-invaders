4a4dbe43c02a59113a9caf60ddd9b2c6e375c980
Marii19 <m.trzeciakiewicz14@gmail.com>
2021-04-29 18:46:05 +0200

    update hmlFormula & formulaPrice

--- formula-price.ts
+++ formula-price.ts
@@ -1,7 +1,43 @@
+import {hmlFormula} from './hml-formula'
+
 export class formulaPrice{
     conjunctions: number;
     positiveDeepBranches: number;
     positiveFlatBranches: number;
     negations: number;
     negationsHeight: number;
+
+    constructor(formula: string){
+        this.conjunctions = this.calculateConjunctions(formula);
+        this.positiveDeepBranches = this.calculatePositiveDeepBranches(formula);
+        this.positiveFlatBranches = this.calculatePositiveFlatBranches(formula);
+        this.negations = this.calculateNegations(formula);
+        this.negationsHeight = this.calculateNegationsHeight(formula);
+    }
+
+
+    calculateConjunctions(formula: string){
+
+        return 0;
+    }
+
+    calculatePositiveDeepBranches(formula: string){
+
+        return 0;
+    }
+
+    calculatePositiveFlatBranches(formula: string){
+
+        return 0;
+    }
+
+    calculateNegations(formula: string){
+
+        return 0;
+    }
+
+    calculateNegationsHeight(formula: string){
+
+        return 0;
+    }
 }
--- hml-formula.ts
+++ hml-formula.ts
@@ -7,14 +7,15 @@ export class hmlFormula{
     formula: string;
     price: formulaPrice;
 
+    /**
+     * Constructor for the hmlFormula
+     * Calculates formula price immediately
+     * @param formula 
+     */
     constructor(formula: string ){
         this.formula = formula;
+        this.price = new formulaPrice(this.formula);
     }
 
-    /**
-     * Calculates price for the hml formula
-     */
-    calculatePrice(){
-        
-    }
+    
 }
